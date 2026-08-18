a906a4b6530c6e2977ade79d9f4f70694db059ea
zali7372-rgb <zalikapro99@gmail.com>
2026-08-18 13:30:24 +0200

    Update build.gradle.kts

--- NOVA-3.0-full/NOVA-3.0/app/build.gradle.kts
+++ NOVA-3.0-full/NOVA-3.0/app/build.gradle.kts
@@ -3,6 +3,55 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "hu.novamobile"; compileSdk = 35
-    defaultConfig { applicationId = "hu.novamobile"; minSdk = 26; targetSdk = 35; versionCode = 30; versionName = "3.0" }
+android {
+    namespace = "hu.novamobile"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "hu.novamobile"
+        minSdk = 26
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "3.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
